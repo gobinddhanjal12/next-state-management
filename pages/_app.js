441d8f64d9a2625f38083ff00eaa27dc54033e9b
gobinddhanjal12 <g40294431@gmail.com>
2025-02-25 15:35:48 +0530

    added loader provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import { LoaderProvider } from "@/context/LoaderContext";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <LoaderProvider>
+        <Component {...pageProps} />
+      </LoaderProvider>
     </Provider>
   );
 }
